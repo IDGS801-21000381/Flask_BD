9a137e411a828c6073dfbd34ac5033133984d7e6
IDGS801-21000381 <sunmookmp@gmail.com>
2024-02-28 18:50:32 -0600

    "AEJA 28/02/2024 "

--- main.py
+++ main.py
@@ -56,62 +56,7 @@ def alumnos2():
         mensaje='Bienvenido {}'.format(nom)
         flash(mensaje)
     return render_template("alumnos2.html", form=alumno_clase, nom = nom, apa = apa, ama = ama)
- 
-@app.route("/maestros")
-def maestros():
-   
-        return render_template("maestros.html")
-   
-@app.route("/hola")
-def hola():
-    return "<h1>Saludos desde Hola prueba nueva </h1>"
- 
-@app.route("/saludo")
-def saludo():
-    return"<h1>Saludos desde Saludo</h1>"
- 
-@app.route("/nombre/<string:nom>")
-def nombre(nom):
-    return "Hola: "+nom
- 
-@app.route("/numero/<int:n1>")
-def numero(n1):
-    return "Numero: {}" .format(n1)
- 
-@app.route("/user/<int:id>/<string:nom>")
-def func(id,nom):
-    return "ID: {} Nombre: {}".format(id,nom)
-   
-@app.route("/suma/<float:n1>/<float:n2>")
-def func2(n1,n2):
-    return "La suma {} + {} = {}".format(n1,n2,n1+n2)  
- 
-@app.route("/default")
-@app.route("/default/<string:d>")
-def func3(d="Dario"):
-    return "El nombre de User es "+d
- 
-@app.route("/calcular",methods=["GET","POST"])
-def calcular():
-    if request.method=="POST":
-        num1=request.form.get("n1")
-        num2=request.form.get("n2")
-        return"La multiplicacion de {} x {} = {}".format(num1,num2,str(int(num1)*int(num2)))
-    else:
-        return'''
-            <form action="/calcular" method="POST">
-                <label>N1:</label>
-                <input type="text" name="n1"><br>
-                <label>N2:</label>
-                <input type="text" name="n2"><br>
-                <input type="submit"/>
-            </form>
-'''
-@app.route("/OperasBas")
-def operas():
-   
-    return render_template("OperasBas.html")
- 
+  
 @app.route("/resultado",methods=["GET","POST"])
 def resul():
     if request.method=="POST":
@@ -131,6 +76,13 @@ def index():
           db.session.add(alum)
           db.session.commit()
      return render_template('index.html', form=create_form)
+
+@app.route("/ABC_Completo",methods=["GET","POST"])
+def ABCompleto():
+     alum_form=forms.UserForm2(request.form)
+     alumno=Alumnos.query.all()
+
+     return render_template("ABC_Completo.html",alumno=alumno)
 
 if __name__== "__main__":
     csrf.init_app(app)
